0cf41f2dd5f946a3d8703e2930c3b5777dd1dc24
Gtlions Lai <gtlions.l@qq.com>
2020-03-26 13:55:23 +0800

    u

--- backup.sh
+++ backup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd ${CURRENTDIR}
+rm -rf ${CURRENTDIR}/backup/*
+rm -rf ${CURRENTDIR}/oh-my-zsh ${CURRENTDIR}/ssh
+
 cd ~/.oh-my-zsh && git pull
 cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull
 cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull
 
-cd ${CURRENTDIR}
-rm -rf ${CURRENTDIR}/backup/*
+cd ${CURRENTDIR} ; rsync --progress --delete -av ~/.oh-my-zsh . --exclude-from=excludepath.txt && mv .oh-my-zsh oh-my-zsh && tar -zcf oh-my-zsh.tar.gz oh-my-zsh && mv oh-my-zsh.tar.gz ${CURRENTDIR}/backup/
+cd ${CURRENTDIR} ; rsync --progress --delete -av ~/.ssh . --exclude-from=excludepath.txt && mv .ssh ssh && tar -zcf ssh.tar.gz ssh && mv ssh.tar.gz ${CURRENTDIR}/backup/
 
-rsync --progress --delete -av ~/.oh-my-zsh . --exclude-from=excludepath.txt && mv .oh-my-zsh oh-my-zsh && tar -zcf oh-my-zsh.tar.gz oh-my-zsh && mv oh-my-zsh.tar.gz ${CURRENTDIR}/backup/
-rsync --progress --delete -av ~/.ssh . --exclude-from=excludepath.txt && mv .ssh ssh && tar -zcf ssh.tar.gz ssh && mv ssh.tar.gz ${CURRENTDIR}/backup/
-
-cp ~/.gitconfig ${CURRENTDIR}/backup/gitconfig
-cp ~/.gitignore ${CURRENTDIR}/backup/gitignore
-cp ~/.zshrc ${CURRENTDIR}/backup/zshrc
-cp /etc/profile ${CURRENTDIR}/backup/profile
+cd ${CURRENTDIR} ; cp ~/.gitconfig ${CURRENTDIR}/backup/gitconfig
+cd ${CURRENTDIR} ; cp ~/.gitignore ${CURRENTDIR}/backup/gitignore
+cd ${CURRENTDIR} ; cp ~/.zshrc ${CURRENTDIR}/backup/zshrc
+cd ${CURRENTDIR} ; cp /etc/profile ${CURRENTDIR}/backup/profile
