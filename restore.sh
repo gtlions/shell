353a34f4d3122c459901834009127c17d69e87ac
Gtlions Lai <gtlions.l@qq.com>
2020-03-27 20:10:28 +0800

    u

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${CURRENTDIR}
-rm -rf ~/.oh-my-zsh;cp -r ${CURRENTDIR}/backup/oh-my-zsh ~/.oh-my-zsh
-rm -rf ~/.ssh;cp -r ${CURRENTDIR}/backup/ssh ~/.ssh
+cd ${CURRENTDIR}/backup/ && tar -zxf oh-my-zsh.tar.gz && rm -rf ~/.oh-my-zsh;mv ${CURRENTDIR}/backup/oh-my-zsh ~/.oh-my-zsh
+cd ${CURRENTDIR}/backup/ && tar -zxf ssh.tar.gz && rm -rf ~/.ssh;mv ${CURRENTDIR}/backup/ssh ~/.ssh
 rm -rf ~/.gitconfig;cp ${CURRENTDIR}/backup/gitconfig ~/.gitconfig
 rm -rf ~/.gitignore;cp ${CURRENTDIR}/backup/gitignore ~/.gitignore
 rm -rf ~/.zshrc;cp ${CURRENTDIR}/backup/zshrc ~/.zshrc
