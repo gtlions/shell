6008e7e7b5f8c7858cfdbc0db40626b27bc18f0c
Gtlions Lai <gtlions.l@qq.com>
2020-03-30 10:43:59 +0800

    u

--- DirEvnScript/gtlionsInit.sh
+++ DirEvnScript/gtlionsInit.sh
@@ -3,13 +3,13 @@ export GTLIONSBASE=/usr/local/gtlions
 CURRENTPATH=$(cd `dirname $0`; pwd)
 
 # alias
-source ${GTLIONSBASE}/apps/shell/envSource/alias.sh
+source ${GTLIONSBASE}/apps/shell/DirEvnScript/alias.sh
 
 # pyenv
-source ${GTLIONSBASE}/apps/shell/envSource/pyenv.sh
+source ${GTLIONSBASE}/apps/shell/DirEvnScript/pyenv.sh
 
 # golang
 source ${GTLIONSBASE}/apps/golang/gosource.sh
 
 # pgdb10
-source ${GTLIONSBASE}/apps/shell/envSource/pgdg10.sh
+# source ${GTLIONSBASE}/apps/shell/DirEvnScript/pgdg10.sh
