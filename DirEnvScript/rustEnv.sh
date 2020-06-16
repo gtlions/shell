54aebc6319199ec0e0001e62dc8a17047d3ed8ac
Gtlions Lai <gtlions.l@qq.com>
2020-06-16 10:53:14 +0800

    u

--- DirEnvScript/rustEnv.sh
+++ DirEnvScript/rustEnv.sh
@@ -4,6 +4,7 @@ RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 mkdir -p $HOME/.cargo; rm -rf $HOME/.cargo/config
 cp ${CURRENTPATH}/rust.cargo.config $HOME/.cargo/config
+source /root/.cargo/env
 # export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
 # 清华大学
 # RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
