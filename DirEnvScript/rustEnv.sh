678e542dbd853276581e8b32f8848b7aae390057
Gtlions Lai <gtlions.l@qq.com>
2020-06-16 09:16:35 +0800

    u

--- DirEnvScript/rustEnv.sh
+++ DirEnvScript/rustEnv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export NODEBASE=/usr/local/node
 CURRENTPATH=$(cd `dirname $0`; pwd)
-export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
-export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+# 清华大学
+# RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+# 中国科学技术大学
+# RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+# 上海交通大学
+# RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static/
